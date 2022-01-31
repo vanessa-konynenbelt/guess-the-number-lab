242f82d989fa7fde9f7f594405e24f7b1006bc2f
Vanessa Konynenbelt <konyn005@umn.edu>
2022-01-31 08:11:19 -0600

    remove debugging alerts

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ const game = {
     this.smallestNum = parseInt(prompt(`Enter your choice for a  lower limit`))
   },
 
-  getNum: function() { //why does this not return a value in bounds when I specify bounds... 
-    //alert(`This is biggest: ${this.biggestNum} and smallest: ${this.smallestNum}`)
+  getNum: function() { 
     this.secretNum = Math.floor(Math.random() * 
     (this.biggestNum - this.smallestNum + 1)) + this.smallestNum 
-   // alert(`This is rando num ${this.secretNum}`)
   }, 
 
   getGuess: function() {
@@ -31,7 +29,6 @@ const game = {
   
   render: function(){
     let dir = null
-    alert(`This is render secret num ${this.secretNum} and your guess ${this.guess}`)
     if(this.guess === this.secretNum){ //correct guess! 
       alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses! Way to go!`)
       return 
